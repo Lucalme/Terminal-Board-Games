512dce799199d535e82e68594a954fe74431d07e
Margiotta Luca <luca.margiotta.etu@a10p29.fil.univ-lille.fr>
2025-01-25 14:46:32 +0100

    pas de changements?

--- test/board/BoardTests.java
+++ test/board/BoardTests.java
@@ -3,11 +3,12 @@ package board;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
+
 public class BoardTests{
 
     @Test
     public void AtLeastOneNonWaterTileOnBoard(){
         Board board = new Board();
-        AssertTrue(Board.getTiles().size() > 0);
+        assertTrue(board.getTiles().size() > 0);
     }
 }
