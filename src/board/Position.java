f4702b4fdcfd58626943237f644fa8da8fcb80e2
Mabrouk Walid <walid.mabrouk.etu>
2025-01-25 23:20:47 +0100

    modified Position added getters

--- src/board/Position.java
+++ src/board/Position.java
@@ -3,5 +3,17 @@ package board;
 public class Position {
     private final int x;
     private final int y;
-    
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
 }
