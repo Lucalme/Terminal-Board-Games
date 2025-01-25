a04b8f32502e097266ff328c8126ba3fce11518f
Margiotta Luca <luca.margiotta.etu@a10p29.fil.univ-lille.fr>
2025-01-25 21:39:13 +0100

    correction d'un test

--- test/board/BoardTests.java
+++ test/board/BoardTests.java
@@ -27,21 +27,24 @@ public class BoardTests{
     public void NoIsolatedTiles()
     {
         Board board= new Board();
-        for(Map.Entry<int[], Tile> entry : board.getTiles().entrySet())
+        HashMap<int[], Tile> entries = board.getTiles(); 
+
+        for(Map.Entry<int[], Tile> entry : entries.entrySet())
         {
             Boolean hasNeighbour = false;
             for(Directions dir : Directions.values())
             {
-                // if(entry.getKey()[0] + dir.X < 0 || entry.getKey()[0] + dir.X >= board.SizeX() || entry.getKey()[1] + dir.Y < 0 || entry.getKey()[1] + dir.Y >= board.SizeY())
-                // {
-                //     continue;
-                // }
-               if(board.getTiles().containsKey(new int[] {entry.getKey()[0] + dir.X, entry.getKey()[1] + dir.Y}))
-               {
-                   hasNeighbour = true;
-                   break;
-               }
+                if(entry.getKey()[0] + dir.X < 0 || entry.getKey()[0] + dir.X >= board.SizeX() || entry.getKey()[1] + dir.Y < 0 || entry.getKey()[1] + dir.Y >= board.SizeY())
+                {
+                    continue;
+                }
+                for(int[] key : entries.keySet()){
+                    if(key[0] == entry.getKey()[0]+dir.X && key[1] == entry.getKey()[1] + dir.Y){
+                        hasNeighbour = true;
+                    }
+                }
             }
+            
             assertTrue(hasNeighbour);
         }
 
