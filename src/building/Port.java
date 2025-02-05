46215838ebf749d25c549d8ff65f64101c9addc5
Bilal Saaoud <bilal.saaoud.etu@univ-lille.fr>
2025-02-05 04:40:52 +0100

    add documentation to port

--- src/building/Port.java
+++ src/building/Port.java
@@ -1,11 +1,23 @@
 package building;
 
-
+/**
+ * Represents a Port building in the game.
+ * The Port building enables advantageous resource exchanges.
+ */
 public class Port extends Building {
+    
+    /**
+     * Constructs a Port building with a default size of 1.
+     */
     public Port() {
         super(1);
     }
 
+    /**
+     * Defines the effect of the Port building.
+     * 
+     * @return a description of the port's effect
+     */
     @Override
     public String effect() {
         return "The port allows advantageous resource exchanges.";
