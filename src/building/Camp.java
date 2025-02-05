a6dc3e02ccb209337bf7292e4b579c43c3afc5af
Bilal Saaoud <bilal.saaoud.etu@univ-lille.fr>
2025-02-05 04:39:39 +0100

    add documentation to camp

--- src/building/Camp.java
+++ src/building/Camp.java
@@ -1,15 +1,31 @@
 package building;
 
-
+/**
+ * Represents a Camp building in the game.
+ * The Camp building enhances resource production.
+ */
 public class Camp extends Building {
     
+    /**
+     * The number of warriors stationed in the camp.
+     */
     private int warriors;
 
+    /**
+     * Constructs a Camp building with the specified number of warriors.
+     * 
+     * @param warriors the number of warriors in the camp
+     */
     public Camp(int warriors) {
         super(warriors);
         this.warriors = warriors;
     }
 
+    /**
+     * Defines the effect of the Camp building.
+     * 
+     * @return a description of the camp's effect
+     */
     @Override
     public String effect() {
         return "The camp improves resource production.";
