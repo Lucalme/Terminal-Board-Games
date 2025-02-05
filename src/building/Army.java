08f89454dae5d714511798154f0e32de5accfee8
Bilal Saaoud <bilal.saaoud.etu@univ-lille.fr>
2025-02-05 04:38:56 +0100

    add documentation to army

--- src/building/Army.java
+++ src/building/Army.java
@@ -1,14 +1,31 @@
 package building;
 
-
+/**
+ * Represents an Army building in the game.
+ * The Army building is responsible for maintaining a number of warriors ready for battle.
+ */
 public class Army extends Building {
+    
+    /**
+     * The number of warriors in the army.
+     */
     private int warriors;
 
+    /**
+     * Constructs an Army building with the specified number of warriors.
+     * 
+     * @param warriors the number of warriors in the army
+     */
     public Army(int warriors) {
         super(warriors);
         this.warriors = warriors;
     }
 
+    /**
+     * Defines the effect of the Army building.
+     * 
+     * @return a description of the army's effect
+     */
     @Override
     public String effect() {
         return "The army with " + warriors + " warriors is ready for battle.";
