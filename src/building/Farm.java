32e7ed6374f3af3cbe182dcea760b9f045b17799
Bilal Saaoud <bilal.saaoud.etu@univ-lille.fr>
2025-02-05 04:40:21 +0100

    add documentation to farm

--- src/building/Farm.java
+++ src/building/Farm.java
@@ -1,11 +1,23 @@
 package building;
 
-
+/**
+ * Represents a Farm building in the game.
+ * The Farm building produces basic resources for the settlement.
+ */
 public class Farm extends Building {
+    
+    /**
+     * Constructs a Farm building with a default size of 1.
+     */
     public Farm() {
         super(1);
     }
 
+    /**
+     * Defines the effect of the Farm building.
+     * 
+     * @return a description of the farm's effect
+     */
     @Override
     public String effect() {
         return "The farm produces basic resources.";
