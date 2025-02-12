ac3037777f4f627feab26dfd2070680b9cde93a0
bilal.saaoud.etu <bilal123saaoud@gmail.com>
2025-02-12 17:28:42 +0100

    update Building

--- src/building/Port.java
+++ src/building/Port.java
@@ -1,25 +1,31 @@
 package building;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Represents a Port building in the game.
  * The Port building enables advantageous resource exchanges.
  */
 public class Port extends Building {
-    
-    /**
-     * Constructs a Port building with a default size of 1.
-     */
+
     public Port() {
-        super(1);
+        super(1, getDefaultCost());
     }
 
-    /**
-     * Defines the effect of the Port building.
-     * 
-     * @return a description of the port's effect
-     */
     @Override
     public String effect() {
         return "The port allows advantageous resource exchanges.";
+    }
+
+    public static boolean canBeBuilt(boolean isAdjacentToSea) {
+        return isAdjacentToSea;
+    }
+
+    private static Map<String, Integer> getDefaultCost() {
+        Map<String, Integer> cost = new HashMap<>();
+        cost.put("Wood", 1);
+        cost.put("Sheep", 2);
+        return cost;
     }
 }
