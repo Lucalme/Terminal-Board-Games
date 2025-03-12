6050f9afe0e2a4f20ed5cff85985c89e418a6a6d
Mabrouk Walid <walid.mabrouk.etu@a15p18.fil.univ-lille.fr>
2025-03-12 10:38:17 +0100

    objectives.java

--- src/Objectives/Objectives.java
+++ src/Objectives/Objectives.java
@@ -1,6 +1,8 @@
 package Objectives;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import player.Player;
 
@@ -11,5 +13,48 @@ public class Objectives {
     public Objectives(){
         playerObjectives = new HashMap<>();
         objectivesAchieved = new HashMap<>();
+    }
+
+    /** 
+     * Définit l'objectif pour un joueur.
+     * @param player le joueur
+     * @param objectif a atteindre
+     */
+    public void setObjective(Player player, String objective) {
+        playerObjectives.put(player, objective);
+        objectivesAchieved.put(player, false);
+    }
+
+    /**
+     * Marque l'objectif d'un joueur comme atteint.
+     * @param player le joueur
+     */
+    public void achieveObjective(Player player){
+        if (playerObjectives.containsKey(player)){
+            objectivesAchieved.put(player, true);
+        }
+    }
+
+    /**
+     * verifie si le joueur a atteint son objectif
+     * @param player le joueur
+     * @return true si l'objectif est atteint, false sinon
+     */
+    public boolean isObjectiveAchieved(Player player){
+        return objectivesAchieved.getOrDefault(player, false);
+    }
+
+    /**
+     * Détermine les gagnants en fonction des objectifs atteints.
+     * @return Une liste des joueurs ayant atteint leurs objectifs
+     */
+    public List<Player> determineWinners() {
+        List<Player> winners = new ArrayList<>();
+        for (Map.Entry<Player, Boolean> entry : objectivesAchieved.entrySet()) {
+            if (entry.getValue()) {
+                winners.add(entry.getKey());
+            }
+        }
+        return winners;
     }
 }
