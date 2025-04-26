66af2255413cdb477b4c1309c3df99917086673d
Lucalme <46386244+Lucalme@users.noreply.github.com>
2025-04-26 19:50:22 +0200

    Demeter avec options de démarrage

--- src/demeter/DemeterMain.java
+++ src/demeter/DemeterMain.java
@@ -2,7 +2,14 @@ package demeter;
 
 public class DemeterMain {
     public static void main(String[] args){
-        Demeter game = new Demeter(2);
+        int nbOfPlayers = args.length > 0 ? Integer.parseInt(args[0]) : -1;
+        int sizeX = args.length > 1 ? Integer.parseInt(args[1]) : -1;
+        int sizeY = args.length > 2 ? Integer.parseInt(args[2]) : -1;
+        if(nbOfPlayers == -1 || sizeX == -1 || sizeY == -1){
+            System.out.println("Usage : java DemeterMain <NombreDeJoueurs> <TailleXDuPlateau> <TailleYDuPlateau>");
+            return;
+        }
+        Demeter game = new Demeter(nbOfPlayers, sizeX, sizeY);
         game.StartGame();
     }
 }
